266ceb510887367775b0bedd10b342f50673f581
Ullaakut <brendan@glaulabs.com>
2022-06-09 07:35:53 +0200

    Add governance contract and update solidity version in pragmas

---
+++ contracts/dao/governance.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Proposal {
+    enum ProposalStatus{OPEN, CLOSED, DONE}
+
+    uint64 id; // Unique ID for the proposal.
+    string description; // Description of the proposal.
+    string hash; // Proposal file hash on IPFS.
+    uint64 votingPeriod; // Time before the voting closes and proposal is either executed or closed.
+    ProposalStatus status; // Status of the proposal.
+
+    mapping(address => bool) votes; // Votes that were cast for this proposal.
+}
+
+contract DAO {
+    uint64 counter;
+
+    mapping(uint => Proposal) public open;
+    mapping(uint => Proposal) public closed;
+    mapping(uint => Proposal) public done;
+
+    // FIXME: We assume that file was written on IPFS and the proposal is created with its hash.
+    function propose(string hash, string description) public {
+        Proposal p;
+
+        p.id = counter++;
+        p.description = description;
+        p.hash = hash;
+        p.votingPeriod = 24*60*60; // FIXME: Should it be in time units or block counts?
+        p.status = OPEN;
+
+        open[p.id] = p;
+    }
+
+    // FIXME: Make the vote function not public but require certain privileges.
+    function vote(address voterAddr, uint64 pID, bool voteValue) public {
+        pending[pID].votes[voterAddr] = voteValue;
+    }
+
+    // FIXME: Make votes effective when public function is called.
+
+    // FIXME: Hardcode addresses of wallets that can participate in DAO and enforce it.
+}
