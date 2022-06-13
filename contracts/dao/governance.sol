6feef3953e918254f3ffde2b3c7f0bac8be668b8
Ullaakut <brendan@glaulabs.com>
2022-06-13 07:43:56 +0200

    Add abstention as possible vote type

--- contracts/dao/governance.sol
+++ contracts/dao/governance.sol
@@ -15,7 +15,16 @@ contract DAO {
     ]; // Hardcoded array of voter addresses.
     uint64 public constant quorum = 4; // Minimum amount of votes (N/2+1).
 
-    enum ProposalStatus{OPEN, CLOSED, DONE} // Enum for possible proposal statuses.
+    enum VoteType {
+        Against,
+        For,
+        Abstain
+    }
+    enum ProposalStatus{
+        Open,
+        Closed,
+        Done
+    }
     uint64 counter; // Incremental counter for proposal IDs.
     struct Proposal {
         uint64 id; // Unique ID for the proposal.
@@ -43,18 +52,18 @@ contract DAO {
             description : description,
             hash : hash,
             votingPeriod : 24 * 60 * 60, // FIXME: Should it be in time units or block counts?
-            status : ProposalStatus.OPEN
+            status : ProposalStatus.Open
         });
 
         open[id] = p;
     }
 
     // FIXME: Make the vote function not public but require certain privileges.
-    function vote(address voterAddr, uint64 pID, bool voteValue) private {
+    function vote(address voterAddr, uint64 pID, VoteType vote) private {
         // FIXME: This already changes the state, so how to make it so that it's executeVote that does the writing?
         Vote memory v = Vote({
             pID : pID,
-            value : voteValue
+            value : vote
         });
 
         votes[pID].push(v);
